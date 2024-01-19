aa14ff05e1c8977fb227a3de08cbbbe4aecd25e1
HansStieber <hans.stieber@posteo.de>
2024-01-19 17:54:33 +0100

    added login function to connect to backend

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -9,10 +9,33 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-  email: string = '';
+  username: string = '';
   password: string = '';
 
-  login() {
-    
+  async login() {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const raw = JSON.stringify({
+      "username": this.username,
+      "password": this.password
+    });
+
+    const requestOptions: RequestInit = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    };
+
+    try {
+      let resp = await fetch("http://localhost:8000/login/", requestOptions);
+      let json = await resp.json();
+      localStorage.setItem('token', json.token);
+      // Redirect
+      //deactivate form inputs
+    } catch (e) {
+      console.log(e);
+    }
   }
 }
