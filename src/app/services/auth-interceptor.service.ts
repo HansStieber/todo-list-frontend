40c7a3497df6a2557b29344737d98923eddda3a4
HansStieber <hans.stieber@posteo.de>
2024-01-23 14:47:22 +0100

    implemented http interceptor

--- src/app/services/auth-interceptor.service.ts
+++ src/app/services/auth-interceptor.service.ts
@@ -1,9 +1,35 @@
+import { HTTP_INTERCEPTORS, HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable, catchError, throwError } from 'rxjs';
+import { Provider } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthInterceptorService {
+export class AuthInterceptorService implements HttpInterceptor {
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    debugger;
+    const token = localStorage.getItem('token');
+    if (token) {
+      request = request.clone({
+        setHeaders: { Authorization: `Token ${token}` }
+      });
+    }
+
+    return next.handle(request).pipe(catchError((err) => {
+      if (err instanceof HttpErrorResponse) {
+        if (err.status === 401) {
+          this.router.navigateByUrl('/login');
+        }
+      }
+      return throwError(() => err);
+    }));
+  }
 }
+
+export const interceptorProvider: Provider =
+  { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true };
